0a1327ad60612b72f0486edc516ba74da8e674b2
HanziCleon <137362532+HanziCleon@users.noreply.github.com>
2025-09-24 16:24:53 +0700

    Add files via upload

---
+++ app/layout.jsx
@@ -0,0 +1,20 @@
+"use client";
+import "./globals.css";
+import Script from "next/script";
+
+export const metadata = {
+  title: "AnimeStream+",
+  description: "Stream Anime with Google Login",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>
+        {children}
+        {/* Google Identity Services */}
+        <Script src="https://accounts.google.com/gsi/client" async defer />
+      </body>
+    </html>
+  );
+}
